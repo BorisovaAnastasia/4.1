daf854d1183a13532da4e78c164d0e984e56872c
BorisovaAnastasia <borisova.anastasiia@lit1533.ru>
2024-03-31 19:06:23 +0300

    Commit

---
+++ OneDimensionalArray.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+
+public sealed class OneDimensionalArray<T>
+{
+    private Random rnd = new Random();
+    private T[] array;
+    private int _length;
+    private int _size;
+
+    public OneDimensionalArray(int length = 10)
+    {
+        array = new T[length];
+        _length = length;
+        _size = 0;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Array");
+        for (int i = 0; i < _size; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+        Console.WriteLine(" ");
+    }
+
+    public void Add(T element)
+    {
+        if(_size >= _length)
+        {
+            _length = 2 * _length + 1;
+            T[] mass = new T[_length];
+            Array.Copy(array, mass, _size);
+            array = mass;
+        }
+        array[_size] = element;
+        _size++;
+    }
+
+    public void DeleteElement(int index)
+    {
+        if(index <= _size)
+        {
+            T[] arr = new T[_length];
+            for (int i = 0; i < _size-1; i++)
+            {
+                if (i != index)
+                {
+                    arr[i] = array[i];
+                }
+                else
+                {
+                    arr[i] = array[_size-1]; 
+                    array[_size-1] = default(T);
+                }
+            }
+            array = arr;
+            _size = _size - 1;
+        }
+        else
+        {
+            Console.WriteLine("Index is out of range");
+            Console.WriteLine(" ");
+        }
+    }
+
+    public void Sorting()
+    {
+        Array.Sort(array, 0, _size);
+    }
+
+    public void Count_if_and_without_if(Func<T, bool> action = null)
+    {
+        int count = 0;
+        if (action != null)
+        {
+            for(int i = 0; i<_size; i++)
+            {
+                if(action(array[i]))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+        else
+        {
+            Console.WriteLine(_size);
+        }
+    }
+
+    public bool If_One(Func<T, bool> action)
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            if (action(array[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public bool If_All(Func<T, bool> action)
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            if (!action(array[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int Index_First_Element_If(Func<T, bool> action)
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            if (action(array[i]))
+            {
+                Console.WriteLine(array[i] + " Return the index of this element");
+                return i;
+            }
+        }
+        Console.WriteLine("There is no element satisfying the condition. Return 0");
+        return 0;
+    }
+
+    public T[] Do_With_Array(Func<T, T> action)
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            array[i] = action(array[i]);
+        }
+        return array;
+    }
+
+    public void Get_All_Elements_If(Func<T, bool> action)
+    {
+        int c = 0;
+        for (int i = 0; i < _size; i++)
+        {
+            if (action(array[i]))
+            {
+                Console.Write(array[i] + " ");
+                c = 1;
+            }
+        }
+        if (c != 1)
+        { Console.WriteLine("There is no element satisfying the condition."); }
+    }
+
+    public void Print_Reverse()
+    {
+        Console.WriteLine(" ");
+        Console.WriteLine("Reverse array");
+        for (int i = _size-1; i >= 0; i--)
+        {
+            Console.WriteLine(array[i]);
+        }
+        Console.WriteLine(" ");
+    }
+
+    public void Get_From_Index_To_Index(int start, int number)
+    {
+        Console.WriteLine("From index " + start + " " + number + " elements");
+        if (start + number + 1 < _length)
+        {
+            for (int i = start; i < start + number + 1; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine(" ");
+        }
+        else
+        {
+            Console.WriteLine("Index is out of range");
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Linq.Expressions;
+class Progam
+{
+    static void Main()
+    {
+        OneDimensionalArray<int> intArray = new();
+        intArray.Add(5);
+        intArray.Add(209);
+        intArray.Add(17);
+        intArray.Add(1);
+        intArray.Print();
+        intArray.DeleteElement(2);
+        intArray.Print();
+        intArray.Sorting();
+        intArray.Print();
+
+        OneDimensionalArray<string> stringArray = new(5);
+        stringArray.Add("String");
+        stringArray.Add("Nope");
+        stringArray.Add("YES");
+        stringArray.Print();
+        stringArray.Sorting();
+        stringArray.Print();
+        stringArray.DeleteElement(1);
+        stringArray.Print();
+
+        OneDimensionalArray<double> doubleArray = new();
+        doubleArray.Print();
+        doubleArray.Add(6.4);
+        doubleArray.Print();
+        doubleArray.DeleteElement(4);
+        doubleArray.Print();
+        doubleArray.Sorting();
+        doubleArray.Print();
+
+        // intArray.Count_if_and_without_if((x)=>
+        // {
+        //     if (x % 2 == 0)
+        //     {
+        //         return true;
+        //     }
+        //     else
+        //     {
+        //         return false;
+        //     }
+        // });
+
+        // intArray.Count_if_and_without_if();
+
+        // doubleArray.Count_if_and_without_if((x) =>
+        // {
+        //     if (x > -12.356)
+        //     {
+        //         return true;
+        //     }
+        //     else
+        //     {
+        //         return false;
+        //     }
+        // });
+
+        // Console.WriteLine(intArray.If_One((x)=>
+        // {
+        //     if (x < 10)
+        //     {
+        //         return true;
+        //     }
+        //     else
+        //     {
+        //         return false;
+        //     }
+        // }));
+        
+        // Console.WriteLine(intArray.If_All((x) => 
+        // { 
+        //     if (x > 10) 
+        //     { 
+        //         return true; 
+        //     } 
+        //     else 
+        //     { 
+        //         return false; 
+        //     } 
+        // }));
+
+        // int N = intArray.Index_First_Element_If((x) => 
+        // { 
+        //     if (x > 1000) 
+        //     { 
+        //         return true; 
+        //     } 
+        //     else 
+        //     { 
+        //         return false; 
+        //     } 
+        // });
+        
+        // Console.WriteLine(intArray.If_One((x)=>
+        // {
+        //     if (x == 5)
+        //     {
+        //         Console.WriteLine("This element is in the array");
+        //         return true;
+        //     }
+        //     else
+        //     {
+        //         return false;
+        //     }
+        // }));
+    
+        // intArray.Do_With_Array((x) =>
+        // {
+        //     return x*2;
+        // });
+        // intArray.Print();
+
+        // stringArray.Do_With_Array((x) =>
+        // {
+        //     return x + " slovo";
+        // });
+        // stringArray.Print();
+    
+        // intArray.Get_All_Elements_If((x) =>
+        // {
+        //     if (x % 2 == 0)
+        //     {
+        //         return true;
+        //     }
+        //     else
+        //     {
+        //         return false;
+        //     }
+        // });
+    
+        // intArray.Print_Reverse();
+
+        // intArray.Get_From_Index_To_Index(2, 5);
+
+    }
+}
